31b4450fb4b2e94325e58185eb41b9e39254cd81
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-12 14:39:36 +0800

    backend

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+// const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const products = require('./routes/product');
+
+
+app.use(express.json({limit:'50mb'}));
+
+app.use(express.urlencoded({limit: "50mb", extended: true }));
+app.use(cors());
+app.use(cookieParser());
+
+app.use('/api/v1', products);
+
+
+module.exports = app
