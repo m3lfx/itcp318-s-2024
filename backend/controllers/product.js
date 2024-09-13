2b64ca5079e7f4b10f6282c91e83541555a78f1e
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-13 09:47:17 +0800

    pagination

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -2,15 +2,20 @@ const Product = require('../models/product')
 const APIFeatures = require('../utils/apiFeatures');
 exports.getProducts = async (req, res, next) => {
     // const products = await Product.find();
+	const resPerPage = 4;
 	const productsCount = await Product.countDocuments();
 	const apiFeatures = new APIFeatures(Product.find(), req.query).search()
+	apiFeatures.pagination(resPerPage);
 	const products = await apiFeatures.query;
+	let filteredProductsCount = products.length;
     if (!products) 
         return res.status(400).json({message: 'error loading products'})
    return res.status(200).json({
         success: true,
-        count: products.length,
-        products
+        count: productsCount,
+        products,
+		resPerPage,
+		filteredProductsCount
     })
 }
 
--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -3,29 +3,43 @@ import MetaData from './Layout/MetaData'
 import axios from 'axios'
 import Product from './Product/Product'
 import { useParams, useNavigate } from 'react-router-dom'
+import Pagination from 'react-js-pagination'
 
 
 const Home = () => {
     const [products, setProducts] = useState([])
     const [productsCount, setProductsCount] = useState(0)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [resPerPage, setResPerPage] = useState(0)
+    const [filteredProductsCount, setFilteredProductsCount] = useState(0)
     let { keyword } = useParams();
-    const getProducts = async (keyword = '') => {
+    const getProducts = async (page = 1, keyword = '') => {
 
-        let link = `http://localhost:4001/api/v1/products?keyword=${keyword}`
+        let link = `http://localhost:4001/api/v1/products?page=${page}&keyword=${keyword}`
         // link = `http://localhost:4001/api/v1/products?page=${page}&keyword=${keyword}&price[lte]=${price[1]}&price[gte]=${price[0]}`
 
 
         let res = await axios.get(link)
         console.log(res)
         setProducts(res.data.products)
-
+        setResPerPage(res.data.resPerPage)
         setProductsCount(res.data.count)
+        setFilteredProductsCount(res.data.filteredProductsCount)
 
     }
 
     useEffect(() => {
-        getProducts(keyword)
-    }, [keyword]);
+        getProducts(currentPage, keyword)
+    }, [currentPage, keyword]);
+
+    let count = productsCount
+    if (keyword) {
+        count = filteredProductsCount
+    }
+
+    function setCurrentPageNo(pageNumber) {
+        setCurrentPage(pageNumber)
+    }
     return (
         <>
             <MetaData title={'Buy Best Products Online'} />
@@ -37,9 +51,24 @@ const Home = () => {
                         {products && products.map(product => (
                             <Product key={product._id} product={product} />
                         ))}
-                        
+
                     </div>
                 </section>
+                {resPerPage <= count && (
+                        <div className="d-flex justify-content-center mt-5">
+                            <Pagination
+                                activePage={currentPage}
+                                itemsCountPerPage={resPerPage}
+                                totalItemsCount={count}
+                                onChange={setCurrentPageNo}
+                                nextPageText={'Next'}
+                                prevPageText={'Prev'}
+                                firstPageText={'First'}
+                                lastPageText={'Last'}
+                                itemClass="page-item"
+                                linkClass="page-link"
+                            />
+                        </div>)}
             </div>
         </>
 
