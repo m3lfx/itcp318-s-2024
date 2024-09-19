591ba05db3884d2375c4042d68fceafaf2b3af13
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-19 14:20:13 +0800

    filter slider

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -4,6 +4,8 @@ import axios from 'axios'
 import Product from './Product/Product'
 import { useParams, useNavigate } from 'react-router-dom'
 import Pagination from 'react-js-pagination'
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 
 const Home = () => {
@@ -12,10 +14,15 @@ const Home = () => {
     const [currentPage, setCurrentPage] = useState(1)
     const [resPerPage, setResPerPage] = useState(0)
     const [filteredProductsCount, setFilteredProductsCount] = useState(0)
+    const [price, setPrice] = useState([1, 1000]);
     let { keyword } = useParams();
-    const getProducts = async (page = 1, keyword = '') => {
 
-        let link = `http://localhost:4001/api/v1/products?page=${page}&keyword=${keyword}`
+    const createSliderWithTooltip = Slider.createSliderWithTooltip;
+    const Range = createSliderWithTooltip(Slider.Range);
+
+    const getProducts = async (page = 1, keyword = '', price) => {
+
+        let link = `http://localhost:4001/api/v1/products?page=${page}&keyword=${keyword}&price[lte]=${price[1]}&price[gte]=${price[0]}`
         // link = `http://localhost:4001/api/v1/products?page=${page}&keyword=${keyword}&price[lte]=${price[1]}&price[gte]=${price[0]}`
 
 
@@ -29,8 +36,8 @@ const Home = () => {
     }
 
     useEffect(() => {
-        getProducts(currentPage, keyword)
-    }, [currentPage, keyword]);
+        getProducts(currentPage, keyword, price)
+    }, [currentPage, keyword, price]);
 
     let count = productsCount
     if (keyword) {
@@ -48,27 +55,69 @@ const Home = () => {
                 <h1 id="products_heading">Latest Products</h1>
                 <section id="products" className="container mt-5">
                     <div className="row">
-                        {products && products.map(product => (
-                            <Product key={product._id} product={product} />
-                        ))}
+                        {keyword ? (
+                            <>
+                                <div className="col-6 col-md-3 mt-5 mb-5">
+                                    <div className="px-5">
+                                        <Range
+                                            marks={{
+                                                1: `$1`,
+                                                1000: `$1000`
+                                            }}
+                                            min={1}
+                                            max={1000}
+                                            defaultValue={[1, 1000]}
+                                            tipFormatter={value => `$${value}`}
+                                            tipProps={{
+                                                placement: "top",
+                                                visible: true
+                                            }}
+                                            value={price}
+                                            onChange={price => setPrice(price)}
+                                        />
+                                        <hr className="my-5" />
+                                        <div className="mt-5">
+                                            <h4 className="mb-3">
+                                                Categories
+                                            </h4>
+                                          
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                <div className="col-6 col-md-9">
+                                    <div className="row">
+                                        {products.map(product => (
+                                            <Product key={product._id} product={product} col={4} />
+                                        ))}
+                                    </div>
+                                </div>
+                            </>
+                        ) : (
+                            products.map(product => (
+                                <Product key={product._id} product={product} col={3} />
+                            ))
+                        )}
 
                     </div>
                 </section>
+
                 {resPerPage <= count && (
-                        <div className="d-flex justify-content-center mt-5">
-                            <Pagination
-                                activePage={currentPage}
-                                itemsCountPerPage={resPerPage}
-                                totalItemsCount={count}
-                                onChange={setCurrentPageNo}
-                                nextPageText={'Next'}
-                                prevPageText={'Prev'}
-                                firstPageText={'First'}
-                                lastPageText={'Last'}
-                                itemClass="page-item"
-                                linkClass="page-link"
-                            />
-                        </div>)}
+                    <div className="d-flex justify-content-center mt-5">
+                        <Pagination
+                            activePage={currentPage}
+                            itemsCountPerPage={resPerPage}
+                            totalItemsCount={productsCount}
+                            onChange={setCurrentPageNo}
+                            nextPageText={'Next'}
+                            prevPageText={'Prev'}
+                            firstPageText={'First'}
+                            lastPageText={'Last'}
+                            itemClass="page-item"
+                            linkClass="page-link"
+                        />
+                    </div>)}
             </div>
         </>
 
