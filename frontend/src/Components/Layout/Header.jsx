30b158ad2ae482aa5c0f574753c2ca249627ad8b
rommel dalisay <rommelmdalisay@gmail.com>
2024-09-26 13:41:14 +0800

    header

--- frontend/src/Components/Layout/Header.jsx
+++ frontend/src/Components/Layout/Header.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import '../../App.css'
 import Search from './Search'
-import { Link } from 'react-router-dom'
+
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
+import { getUser, logout } from '../../utils/helpers'
 
 
 
 const Header = () => {
+	const [user, setUser] = useState({})
+    const navigate = useNavigate()
+
+
+    const logoutHandler = () => {
+        logout(navigate('/login'));
+        toast.success('log out', {
+            position: 'bottom-right'
+        });
+    }
+	useEffect(() => {
+        setUser(getUser())
+    }, []);
+
 	return (
 		<>
 			<nav className="navbar row">
@@ -31,7 +50,32 @@ const Header = () => {
 				</div> */}
 				<Search />
 				<div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-				<Link to="/login" className="btn ml-4" id="login_btn">Login</Link>
+				{user ? (<div className="ml-4 dropdown d-inline">
+                        <Link to="#!" className="btn dropdown-toggle text-white mr-4" type="button" id="dropDownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <figure className="avatar avatar-nav">
+                                <img
+                                    src={user.avatar && user.avatar.url}
+                                    alt={user && user.name}
+                                    className="rounded-circle"
+                                />
+                            </figure>
+                            <span>{user && user.name}</span>
+                        </Link>
+
+                        <div className="dropdown-menu" aria-labelledby="dropDownMenuButton">
+                            {user && user.role === 'admin' && (
+                                <Link className="dropdown-item" to="/dashboard">Dashboard</Link>
+                            )}
+                            <Link className="dropdown-item" to="/orders/me">Orders</Link>
+                            <Link className="dropdown-item" to="/me">Profile</Link>
+
+                            <Link
+                                className="dropdown-item text-danger" to="/" onClick={logoutHandler}
+                            >
+                                Logout
+                            </Link>
+                        </div>
+                    </div>) : <Link to="/login" className="btn ml-4" id="login_btn">Login</Link>}
 
 					<span id="cart" className="ml-3">Cart</span>
 					<span className="ml-1" id="cart_count">2</span>
