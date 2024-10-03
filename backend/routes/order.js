bfd770acb3c9d06a93e7d572021f9cc2b3d05db8
rommel dalisay <rommelmdalisay@gmail.com>
2024-10-03 14:01:40 +0800

    create order

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -2,4 +2,7 @@ const express = require('express')
 const router = express.Router();
 
 const { newOrder, } = require('../controllers/order')
+const { isAuthenticatedUser, } = require('../middlewares/auth')
+
+router.post('/order/new', isAuthenticatedUser, newOrder);
 module.exports = router;
