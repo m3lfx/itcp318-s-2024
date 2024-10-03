5d27d2efc212af5791ca672bb8d742214b10d3c7
rommel dalisay <rommelmdalisay@gmail.com>
2024-10-03 13:47:07 +0800

    confirm order

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,7 @@ import ForgotPassword from './Components/User/ForgotPassword';
 import NewPassword from './Components/User/NewPassword';
 import Cart from './Components/Cart/Cart';
 import Shipping from './Components/Cart/Shipping';
+import ConfirmOrder from './Components/Cart/ConfirmOrder';
 import axios from 'axios';
 function App() {
   const [state, setState] = useState({
@@ -114,6 +115,7 @@ function App() {
           <Route path="/password/reset/:token" element={<NewPassword />} exact="true" />
           <Route path="/cart" element={<Cart cartItems={state.cartItems} addItemToCart={addItemToCart} removeItemFromCart={removeItemFromCart} />} exact="true" />
           <Route path="/shipping" element={<Shipping shipping={state.shippingInfo} saveShippingInfo={saveShippingInfo} />} />
+          <Route path="/confirm" element={<ConfirmOrder cartItems={state.cartItems} shippingInfo={state.shippingInfo} />} />
         </Routes>
       </Router>
       <Footer />
