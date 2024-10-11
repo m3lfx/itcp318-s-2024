8c56c9b761cd8567b5b910349ca17f0f6003a004
rommel dalisay <rommelmdalisay@gmail.com>
2024-10-11 08:43:02 +0800

    aggregates

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -7,7 +7,11 @@ const { newOrder,
     allOrders,
     updateOrder,
     deleteOrder,
- } = require('../controllers/order')
+    totalOrders,
+    totalSales,
+    customerSales,
+    salesPerMonth,
+} = require('../controllers/order')
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
 
 router.post('/order/new', isAuthenticatedUser, newOrder);
@@ -15,6 +19,9 @@ router.get('/orders/me', isAuthenticatedUser, myOrders);
 router.get('/order/:id', isAuthenticatedUser, getSingleOrder);
 router.route('/admin/orders/').get(isAuthenticatedUser, authorizeRoles('admin'), allOrders);
 router.route('/admin/order/:id').put(isAuthenticatedUser, updateOrder).delete(isAuthenticatedUser, deleteOrder);
-
+router.get('/admin/total-orders', totalOrders);
+router.get('/admin/total-sales', totalSales);
+router.get('/admin/customer-sales', customerSales);
+router.get('/admin/sales-per-month', salesPerMonth);
 
 module.exports = router;
